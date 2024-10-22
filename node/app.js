03eac2823a1c07676f8b343f9e2d7ced0ad49f76
Vikramathithan M <155438677+imvikramathithan@users.noreply.github.com>
2024-10-22 08:50:14 +0530

    Update app.js

--- node/app.js
+++ node/app.js
@@ -3,7 +3,7 @@ const http=require('http')
 const hostname='127.0.0.1'
 const port=3000
 const server=http.createServer((req,res)=>{
- res.end('ani sekar')
+ res.end('Hello World')
 })
 server.listen(port,hostname,()=>{
  console.log(`server running at http://${hostname}:${port}`)
